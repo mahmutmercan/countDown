11ed5c61de95711b6851d7178d5d0765f8a1c39e
mahmutmercan <56167628+mahmutmercan@users.noreply.github.com>
2021-01-26 15:55:43 +0300

    timerHandler

--- countDown/Enums.swift
+++ countDown/Enums.swift
@@ -8,20 +8,10 @@
 import Foundation
 import UIKit
 
-//enum Seconds: Int, CaseIterable {
-//    case secondsTime0, secondsTime1, secondsTime2, secondsTime3
-//    static let allValues = [1, 5, 10, 30, 59, 5*60] as [Any]
-//}
-//
-//enum WorkTime: Int, CaseIterable {
-//    case workTime0, workTime1, workTime2, workTime3
-//    static let allValues = [1, 5, 10, 30, 59, 5*60] as [Any]
-//}
-
 enum workTimeSchedule: String, CaseIterable {
-    case fiveSecond
-    case tenSecond
-    case thirtySecond
+    case fiveSeconds
+    case tenSeconds
+    case thirtySeconds
     case oneMinute
     case fiveMinutes
     case tenMinutes
@@ -30,11 +20,11 @@ enum workTimeSchedule: String, CaseIterable {
     
     var workTimeText: String {
         switch self {
-        case .fiveSecond:
+        case .fiveSeconds:
             return "5sn"
-        case .tenSecond:
+        case .tenSeconds:
             return "10sn"
-        case .thirtySecond:
+        case .thirtySeconds:
             return "30sn"
         case .oneMinute:
             return "1dk"
@@ -51,11 +41,11 @@ enum workTimeSchedule: String, CaseIterable {
     
     var workTimeToSeconds: Int {
         switch self {
-        case .fiveSecond:
+        case .fiveSeconds:
             return 5
-        case .tenSecond:
+        case .tenSeconds:
             return 10
-        case .thirtySecond:
+        case .thirtySeconds:
             return 30
         case .oneMinute:
             return 60
@@ -70,3 +60,120 @@ enum workTimeSchedule: String, CaseIterable {
         }
     }
 }
+
+enum restPeriodTimeSchedule: String, CaseIterable {
+    case fiveSeconds
+    case tenSeconds
+    case thirtySeconds
+    case oneMinute
+    case twoMinutes
+    case threeMinutes
+    case fiveMinutes
+    case tenMinutes
+    case thirtyMinutes
+    
+    var restPeriodTimeText: String {
+        switch self {
+        case .fiveSeconds:
+            return "5 sn"
+        case .tenSeconds:
+            return "10 sn"
+        case .thirtySeconds:
+            return "30 sn"
+        case .oneMinute:
+            return "1 dk"
+        case .twoMinutes:
+            return "2 dk"
+        case .threeMinutes:
+            return "3 dk"
+        case .fiveMinutes:
+            return "5 dk"
+        case .tenMinutes:
+            return "10 dk"
+        case .thirtyMinutes:
+            return "30 dk"
+        }
+    }
+    
+    var restPeriodTimeToSeconds: Int {
+        switch self {
+        case .fiveSeconds:
+            return 5
+        case .tenSeconds:
+            return 10
+        case .thirtySeconds:
+            return 30
+        case .oneMinute:
+            return 60
+        case .twoMinutes:
+            return 2*60
+        case .threeMinutes:
+            return 3*60
+        case .fiveMinutes:
+            return 5*60
+        case .tenMinutes:
+            return 10*60
+        case .thirtyMinutes:
+            return 30*60
+        }
+    }
+}
+
+enum roundSchedule: String, CaseIterable {
+    case onetime
+    case twice
+    case threeTimes
+    case fourTimes
+    case fiveTimes
+    case tenTimes
+    case fifteenTimes
+    case twentyTimes
+    case fiftyTimes
+    
+    var roundTimesText: String {
+        switch self {
+        case .onetime:
+            return "1 Round"
+        case .twice:
+            return "2 Round"
+        case .threeTimes:
+            return "3 Round"
+        case .fourTimes:
+            return "4 Round"
+        case .fiveTimes:
+            return "5 Round"
+        case .tenTimes:
+            return "10 Round"
+        case .fifteenTimes:
+            return "15 Round"
+        case .twentyTimes:
+            return "20 Round"
+        case .fiftyTimes:
+            return "50 Round"
+        }
+    }
+    
+    var roundTimes: Int {
+        switch self {
+        case .onetime:
+            return 1
+        case .twice:
+            return 2
+        case .threeTimes:
+            return 3
+        case .fourTimes:
+            return 4
+        case .fiveTimes:
+            return 5
+        case .tenTimes:
+            return 10
+        case .fifteenTimes:
+            return 15
+        case .twentyTimes:
+            return 20
+        case .fiftyTimes:
+            return 50
+        }
+    }
+}
+
--- countDown/ViewController.swift
+++ countDown/ViewController.swift
@@ -10,62 +10,78 @@ import UIKit
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     var timer: Timer?
+    var timerRest: Timer?
     var count: Int = 0
     var isTimerCounting: Bool = false
-    
-    var totalHour: Int!
-    var totalMinutes: Int!
-    var totalSeconds: Int!
     var totalTime: Int! = 0
-    
+    var restTime: Int! = 5
+    var workTime: Int! = 5
+    var round: Int! = 1
     
     @IBOutlet weak var startStopButton: UIButton!
     @IBOutlet weak var restartButton: UIButton!
     @IBOutlet weak var currentTimeLabel: UILabel!
     @IBOutlet weak var roundLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var hourTextField: UITextField!
-    @IBOutlet weak var secondsTextField: UITextField!
-    @IBOutlet weak var minutesTextField: UITextField!
     @IBOutlet weak var workTimePicker: UIPickerView!
+    @IBOutlet weak var titleStackView: UIStackView!
+    @IBOutlet weak var restTitleStackViewLabel: UILabel!
+    @IBOutlet weak var roundTimeTitleStackViewLabel: UILabel!
+    @IBOutlet weak var roundTitleStackViewLabel: UILabel!
     
     override func viewDidLoad() {
         workTimePicker.delegate = self
         workTimePicker.dataSource = self
         super.viewDidLoad()
- 
     }
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+        return 3
     }
-    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return workTimeSchedule.allCases[row].workTimeText
+        if component == 0 {
+            return restPeriodTimeSchedule.allCases[row].restPeriodTimeText
+        } else if component == 1 {
+            return workTimeSchedule.allCases[row].workTimeText
+        } else if component == 2 {
+            return roundSchedule.allCases[row].roundTimesText
+        } else {
+            return workTimeSchedule.allCases[row].workTimeText
+        }
     }
-    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return workTimeSchedule.allCases.count
+        if component == 0 {
+            return restPeriodTimeSchedule.allCases.count
+        } else if component == 1 {
+            return workTimeSchedule.allCases.count
+        } else if component == 2 {
+            return roundSchedule.allCases.count
+        } else {
+            return workTimeSchedule.allCases.count
+        }
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        totalTime = workTimeSchedule.allCases[row].workTimeToSeconds
+        if component == 0 {
+            restTime = restPeriodTimeSchedule.allCases[row].restPeriodTimeToSeconds
+            print(restTime)
+        } else if component == 1 {
+            workTime = workTimeSchedule.allCases[row].workTimeToSeconds
+            print(workTime)
+        } else if component == 2 {
+            round = roundSchedule.allCases[row].roundTimes
+            print(round)
+        } else {
+            totalTime = workTimeSchedule.allCases[row].workTimeToSeconds
+        }
     }
-    
     func getTimeFromInputToInt() -> Int {
-        totalHour = 0
-        totalMinutes = 0
-        totalSeconds = 0
-        
-        totalTime = (totalHour + totalMinutes + totalSeconds)
         return totalTime
     }
-
     func secondsToHoursMinutesSeconds(seconds: Int) -> (Int, Int, Int) {
         let hour = (seconds / 3600)
         let minutes = (seconds % 3600) / 60
         let seconds = (seconds % 60)
         return (hour, minutes, seconds)
     }
-    
     func makeTimeString(hours: Int, minutes: Int, seconds : Int) -> String {
         var timeString = ""
         timeString += String(format: "%02d", hours)
@@ -75,32 +91,57 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         timeString += String(format: "%02d", seconds)
         return timeString
     }
-        
-    @objc func timerCounter(){
-        if count > 0 {
+//    MARK: setupUserSchedule
+    func setupUserSchedule() {
+        if round > 0 {
+            roundLabel.text = "Çalışma Zamanı"
+            count -= 1
+            if count == 0 {
+                round -= 1
+                print("Round232: \(round)")
+                if round == 0 {
+                    timer?.invalidate()
+                    roundLabel.text = "Antrenman Bitti."
+                    count = restTime
+                    roundLabel.text = "Dinlenme Zamanı"
+                    timer?.invalidate()
+                    timerRest = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] (Timer) in
+                        count -= 1
+                        let time = secondsToHoursMinutesSeconds(seconds: count)
+                        let timeString = makeTimeString(hours: time.0, minutes: time.1, seconds: time.2)
+                        currentTimeLabel.text = timeString
+                        if count == 0 {
+                            timerRest?.invalidate()
+                            count = workTime + 1
+                            timerCounter()
+                        }
+                }
+                }
+                print("Round: \(round)")
+            }
+        } else if round == 0 {
             timer?.invalidate()
-//            timer = nil
+            roundLabel.text = "Antrenman Bitti."
+        }
+    }
+    @objc func timerCounter(){
+            timerRest?.invalidate()
+            timerRest = nil
+            timer?.invalidate()
+            timer = nil
             timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] (Timer) in
-                count = count - 1
+                setupUserSchedule()
                 let time = secondsToHoursMinutesSeconds(seconds: count)
                 let timeString = makeTimeString(hours: time.0, minutes: time.1, seconds: time.2)
                 currentTimeLabel.text = timeString
-                if count == 0 {
-                    timer?.invalidate()
-                    currentTimeLabel.text = "Time is out."
-                }
             }
-        }
     }
-    
     @IBAction func startStopTapped(_ sender: Any) {
-        if count == 0 {
-            count = totalTime
-        }
         if isTimerCounting == false {
             isTimerCounting = true
             startStopButton.setTitle("STOP", for: .normal)
             startStopButton.setTitleColor(UIColor.red, for: .normal)
+            count = workTime + 1
             timerCounter()
         } else {
             isTimerCounting = false
@@ -109,7 +150,6 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             startStopButton.setTitleColor(UIColor.systemBlue, for: .normal)
         }
     }
-    
     @IBAction func restartTapped(_ sender: Any) {
         let alert = UIAlertController(title: "Reset Timer?", message: "Are you sure you would like to reset the Timer?", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: { (_) in
@@ -125,35 +165,3 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         self.present(alert, animated: true, completion: nil)
     }
 }
-
-
-
-////    MARK: getTime2
-//    func setupUserSchedule(restTime: Int, workTime: Int, numberRepetitions: Int) {
-//
-//        var restTime = restTime
-//        var workTime = workTime
-//        var numberRepetitions = numberRepetitions
-//        if numberRepetitions > 0 {
-//            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
-//                print("\(workTime) Çalışma Süresi")
-//                workTime -= 1
-//                if workTime == 0 {
-//                    Timer.invalidate()
-//                    print("Çalışma Süresi Doldu.")
-//                }
-//            }
-//            if workTime == 0 {
-//                Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
-//                    print("\(restTime) Dinlenme Süresi")
-//                    restTime -= 1
-//                    if restTime == 0 {
-//                        Timer.invalidate()
-//                    }
-//                }
-//            }
-//            numberRepetitions -= 1
-//        } else {
-//            print("Çalışma Bitmiştir.")
-//        }
-//    }
